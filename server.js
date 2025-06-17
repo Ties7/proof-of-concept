a0acf126fa62be3844d5c7b3f6009cc9c39e1e85
Ties7 <97968332+Ties7@users.noreply.github.com>
2025-06-17 14:13:02 +0200

    feat: favorieten post interactie #20

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import express from "express";
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from "liquidjs";
 
+// ----------------------- SERVER SETUP -----------------------
+
 // WHOIS API
 const API_BASE_URL = "";
 
@@ -26,30 +28,128 @@ app.engine("liquid", engine.express());
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set("views", "./views");
 
-// detailpagina
+// ----------------------- LIKE FUNCTIES -----------------------
+
+// in de database staat bij from 'ties-funda', dit zodat je mijn posts kan onderscheiden tussen die van anderen
+const likedFrom = "ties-funda"
+
+async function listLiked() { // door async word de rest van de code niet op stop gezet
+  // maak get request om alle likes op te halen
+  const url = `https://fdnd.directus.app/items/messages?filter={"from":"${likedFrom}"}`; // filtert alleen door 'ties-funda' in de directus api
+  const response = await fetch(url);
+  const responseJSON = await response.json();
+
+  return responseJSON.data;
+}
+
+async function getLiked(id) { // functie die kijkt of een bepaald huis met id al geliket is
+  // maak get request om de likes voor dat huis op te halen met die id
+  const url = `https://fdnd.directus.app/items/messages?filter={"for":"${id}","from":"${likedFrom}"}`; // filtert alleen op 'id' en weer op 'ties-funda'
+  const response = await fetch(url);
+
+  const responseJSON = await response.json();
+  return responseJSON.data[0]; // dit is of de message, of niks (undefined als deze niet bestaat)
+}
+
+async function likeHouse(id) {
+  // check of er al een like post bestaat. zo ja dan hoeven we niks te doen
+  const message = await getLiked(id); // checkt of dit huis al geliked is
+  if (message) return; // bestaat er een like, oftewel komt er iets terug dan niks doen
+
+  // maak post request om nieuwe message aan te maken voor liked huis
+  const url = "https://fdnd.directus.app/items/messages"; // url waar naar gepost gaat worden
+  await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" }, // vertel dat de body json is
+    body: `{"for":"${id}","from":"${likedFrom}"}` // stuurt op wie welk huis liked. 'id' 'ties-funda'
+  });
+}
+
+async function unlikeHouse(id) { // functie die een like van een huis verwijdert
+  const message = await getLiked(id); // check of er een like post bestaat. zo niet dan hoeven we niks te doen
+  if (!message) return; // als er geen like is, oftewel er komt niks terug dan niks doen
+
+  const url = `https://fdnd.directus.app/items/messages/${message.id}`; // maak delete request om de like te verwijderen
+  await fetch(url, { method: "DELETE" }); // delete request
+}
+
+// ----------------------- HUIS FUNCTIES -----------------------
+
+async function listHouses() {
+  // maak get request om alle huizen op te halen
+  const url = "https://fdnd-agency.directus.app/items/f_houses";
+  const response = await fetch(url);
+  const responseJSON = await response.json();
+
+  return responseJSON.data;
+}
+
+async function getHouse(id) {
+  // maak get request om huis op te halen op id
+  const url = `https://fdnd-agency.directus.app/items/f_houses/?filter={"id":"${id}"}&fields=*,gallery.directus_files_id`;
+  const response = await fetch(url);
+  const responseJSON = await response.json();
+
+  return responseJSON.data[0]; // hier komt of een huis terug, of niks (undefined)
+}
+
+// ----------------------- OVERZICHTPAGINA -----------------------
 
 app.get("/", async function (request, response) {
-  const housesResponse = await fetch('https://fdnd-agency.directus.app/items/f_houses')
-
-  const housesResponseJSON = await housesResponse.json()
-
-  response.render("overview-page.liquid", { houses: housesResponseJSON.data });
+  const houses = await listHouses(); // listHouses is een functie die we hergebruiken: "https://fdnd-agency.directus.app/items/f_houses"
+  response.render("overview-page.liquid", { houses: houses });
 });
+
+// ----------------------- FAVORIETEN PAGINA -----------------------
+
+app.get("/favorieten", async function (request, response) {
+  const houses = await listHouses();
+  const liked = await listLiked();
+
+
+  const likedHouses = [];
+  // per like in de lijst met alle likes door de lijst met alle houses gaan. komt er één overeen dan dat huis in de lijst met gelikete huizen toevoegen (pushen)
+  for (const like of liked) {
+    for (const house of houses) {
+      if (like.for == house.id) { // == want getal en string vergelijken. getal == string is true, getal === string is false.
+        likedHouses.push(house);
+        break; // stoppen als er een huis (id) overeenkomt met like (for). dit voor de performance
+      }
+    }
+  }
+
+  response.render("overview-page.liquid", { houses: likedHouses });
+});
+
+// ----------------------- DETAILPAGINA -----------------------
 
 app.get("/huis/:id", async function (request, response) {
-  // const houseResponse = await fetch('https://fdnd-agency.directus.app/items/f_houses')
+  const houseId = request.params.id;
 
-  const house = request.params.id
+  const house = await getHouse(houseId);
+  const liked = await getLiked(houseId);
 
-  const houseResponse = await fetch(`https://fdnd-agency.directus.app/items/f_houses/?filter={"id":"${house}"}`)
-  const houseResponseJSON = await houseResponse.json()  
-  console.log(houseResponseJSON)
+  // !liked als er geen liked is en !!liked als er geen geen liked is oftewel als er wel een like is
+  const isLiked = !!liked;
 
-  response.render("detail-page.liquid", { house: houseResponseJSON.data });
-
-  // response.render('detail-page.liquid', {house: houseResponseJSON.data[0]})
+  response.render("detail-page.liquid", { house: house, liked: isLiked });
 });
 
+// ----------------------- LIKE/UNLIKE API -----------------------
+
+app.post('/huis/:id/like', async function (request, response) { // post request op de route /huis/:id/like
+  const houseId = request.params.id // haalt het id op
+  await likeHouse(houseId) // activeert likeHouse functie om met het id van dat huis een like toe te voegen
+  response.send(200) // stuurt code terug als antwoord (ok)
+})
+
+app.post('/huis/:id/unlike', async function (request, response) { // weer een post request op de route /huis/:id/like
+  const houseId = request.params.id // haalt het id op
+  await unlikeHouse(houseId) // activeert likeHouse functie om met het id van dat huis een like te verwijderen
+  response.send(200) // (ok)
+})
+
+// ----------------------- SERVER STARTEN -----------------------
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
